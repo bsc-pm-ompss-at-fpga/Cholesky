c59d21addb9b24489902793b3b8cf83cdf60efc2
Juan Miguel de Haro <juan.deharoruiz@bsc.es>
2021-06-04 16:05:32 +0200

    First commit with code

---
+++ README.md
@@ -0,0 +1,80 @@
+# Cholesky
+
+**Name**: Cholesky Factorization Kernel  
+**Contact Person**: OmpSs@FPGA Team, ompss-fpga-support@bsc.es  
+**License Agreement**: GPL  
+**Platform**: OmpSs@FPGA
+
+
+### Description
+This application performs a cholesky decomposition/factorization over a square matrix.
+The matrix is distributed by blocks of contiguous memory.
+
+The task implementation requires support for one external library that implements the mathematical operations. Supported ones are:
+ - [Intel MKL](https://software.intel.com/en-us/mkl)
+ - [Open BLAS](http://www.openblas.net/)
+
+### Build instructions
+Clone the repository:
+```
+git clone https://github.com/bsc-pm-ompss-at-fpga/Cholesky.git
+cd cholesky
+```
+
+Build the application binaries:
+```
+make BOARD=zedboard CROSS_COMPILE=arm-linux-gnueabihf-
+```
+##### Build variables
+You can change the build process defining or modifying some environment variables.
+The supported ones are:
+  - `CFLAGS`
+    - `-DUSE_DMA_MEM`. Defining the `USE_DMA_MEM` variable the blocked matrix is allocated in kernel memory instead of user-space memory.
+    - `-DUSE_DOUBLE`. Defining the `USE_DOUBLE` variable the matix elements are of type `double` instead of `float`.
+    - `-DVERBOSE`. Defining the `VERBOSE` variable the application steps are shown meanwhile executed.
+  - `LDFLAGS`
+  - `GCC`. If not defined, the default value is: `gcc`.
+  - `MCC`. If not defined, the default value is: `mcc`. However, for SMP machines we recommend the use of `smpcc`.
+  - `CROSS_COMPILE`
+  - `MKL_DIR`. Installation directory of MKL library. The default value is: `$MKLROOT`.
+    - `MKL_INC_DIR`. Installation directory of includes for MKL library. The default value is: `$MKL_DIR/include`.
+    - `MKL_LIB_DIR`. Installation directory of OS libraries for MKL library. The default value is: `$MKL_DIR/lib`.
+  - `OPENBLAS_DIR`. Installation directory of OpenBLAS library. The default value is: `$OPENBLAS_HOME`.
+    - `OPENBLAS_INC_DIR`. Installation directory of includes for OpenBLAS library. The default value is: `$OPENBLAS_DIR/include`.
+    - `OPENBLAS_LIB_DIR`. Installation directory of OS libraries for OpenBLAS library. The default value is: `$OPENBLAS_DIR/lib`.
+  - `BOARD`. Board option used when generating the bitstreams.
+  - `FPGA_HWRUNTIME`. Hardware Runtime used when generating the bitstreams. The default value is: `POM`.
+  - `FPGA_CLOCK`. Target frequency of FPGA accelerators in the bitstreams. The default value is: `200`.
+  - `FPGA_MEMORY_PORT_WIDTH`. Bit-width of accelerators memory port to access main memory. The default value is: `128`.
+  - `BLOCK_SIZE`. Dimension of matrix blocks that FPGA accelerators deal with. The default value is: `32`.
+  - `SYRK_NUM_ACCS`. Number of FPGA accelerators for syrk task. The default value is: `1`.
+  - `GEMM_NUM_ACCS`. Number of FPGA accelerators for gemm task. The default value is: `1`.
+  - `TRSM_NUM_ACCS`. Number of FPGA accelerators for trsm task. The default value is: `1`.
+  - `FPGA_GEMM_II`. Initiation interval, in cycles, for gemm middle loop. The default value is: `1`.
+  - `FPGA_OTHER_II`. Initiation interval, in cycles, for syrk and trsm middle loop. The default value is: `1`.
+  - `POTRF_SMP`. Use SMP arch for potrf tasks. The default value is: `1`.
+
+Note that in order to compile the application either `MKL_DIR` or `OPENBLAS_DIR` (or the derivate variables) must point to a valid installation.
+
+For example, the build step to cross-compile the application for ARM using the `smpcc` profile and OpenBLAS library may be:
+```
+export MCC=smpcc
+export CROSS_COMPILE=arm-linux-gnueabihf-
+export OPENBLAS_DIR=/opt/install-arm/openblas
+make
+```
+
+
+### Run instructions
+The name of each binary file created by build step ends with a suffix which determines the version:
+ - program-p: performance version
+ - program-i: instrumented version
+ - program-d: debug version
+
+All versions use the same arguments structure:
+```
+./cholesky <matrix size> [<check>]
+```
+where:
+ - `matrix size` is the dimension of the matrices. (Mandatory)
+ - `check` defines if the result must be checked. Default is: TRUE. (Optional)
---
+++ src/cholesky.h
@@ -0,0 +1,167 @@
+/*
+* Copyright (c) 2020, Barcelona Supercomputing Center
+*                     Centro Nacional de Supercomputacion
+*
+* This program is free software: you can redistribute it and/or modify  
+* it under the terms of the GNU General Public License as published by  
+* the Free Software Foundation, version 3.
+*
+* This program is distributed in the hope that it will be useful, but 
+* WITHOUT ANY WARRANTY; without even the implied warranty of 
+* MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU 
+* General Public License for more details.
+*
+* You should have received a copy of the GNU General Public License 
+* along with this program. If not, see <http://www.gnu.org/licenses/>.
+*/
+
+#include <sys/time.h>
+#include <time.h>
+#include <math.h>
+#if USE_MKL
+# include <mkl_lapacke.h>
+# include <mkl.h>
+#elif USE_OPENBLAS
+# include <lapacke.h>
+# include <cblas.h>
+#else
+//NOTE: Cannot throw the error as Vivado HLS will not compile
+//# error No backend library found. See README for more information
+#endif
+
+#ifndef BLOCK_SIZE
+#  error BLOCK_SIZE not defined
+#endif
+#ifndef FPGA_MEMORY_PORT_WIDTH
+#  error FPGA_MEMORY_PORT_WIDTH variable not defined
+#endif
+#ifndef SYRK_NUM_ACCS
+#  error SYRK_NUM_ACCS variable not defined
+#endif
+#ifndef GEMM_NUM_ACCS
+#  error GEMM_NUM_ACCS variable not defined
+#endif
+#ifndef TRSM_NUM_ACCS
+#  error TRSM_NUM_ACCS variable not defined
+#endif
+#ifndef FPGA_OTHER_LOOP_II
+#  error FPGA_OTHER_LOOP_II variable not defined
+#endif
+#ifndef FPGA_GEMM_LOOP_II
+#  error FPGA_GEMM_LOOP_II variable not defined
+#endif
+
+#pragma omp target device(fpga)
+const unsigned int FPGA_GEMM_II = FPGA_GEMM_LOOP_II;
+#pragma omp target device(fpga)
+const unsigned int FPGA_OTHER_II = FPGA_OTHER_LOOP_II;
+const int ts = BLOCK_SIZE; // tile size
+#pragma omp target device(fpga)
+const unsigned int FPGA_PWIDTH = FPGA_MEMORY_PORT_WIDTH;
+const unsigned int SYRK_NUMACCS = SYRK_NUM_ACCS;
+const unsigned int GEMM_NUMACCS = GEMM_NUM_ACCS;
+const unsigned int TRSM_NUMACCS = TRSM_NUM_ACCS;
+
+#if defined(USE_DOUBLE)
+#  define type_t     double
+#  define ELEM_T_STR "double"
+#  define gemm       cblas_dgemm
+#  define trsm       cblas_dtrsm
+#  define trmm       cblas_dtrmm
+#  define syrk       cblas_dsyrk
+#  if USE_MKL
+#    define potrf    dpotrf
+#    define lacpy    dlacpy
+#    define lange    dlange
+#    define larnv    dlarnv
+#  else
+#    define potrf    LAPACK_dpotrf
+#    define lacpy    LAPACK_dlacpy
+#    define lange    LAPACK_dlange
+#    define larnv    LAPACK_dlarnv
+#  endif
+#else
+#  define type_t     float
+#  define ELEM_T_STR "float"
+#  define gemm       cblas_sgemm
+#  define trsm       cblas_strsm
+#  define trmm       cblas_strmm
+#  define syrk       cblas_ssyrk
+#  if USE_MKL
+#    define potrf    spotrf
+#    define lacpy    slacpy
+#    define lange    slange
+#    define larnv    slarnv
+#  else
+#    define potrf    LAPACK_spotrf
+#    define lacpy    LAPACK_slacpy
+#    define lange    LAPACK_slange
+#    define larnv    LAPACK_slarnv
+#  endif
+#endif
+#define CBLAS_MAT_ORDER   CblasColMajor
+#define CBLAS_T           CblasTrans
+#define CBLAS_NT          CblasNoTrans
+#define CBLAS_LO          CblasLower
+#define CBLAS_UP          CblasUpper
+#define CBLAS_LF          CblasLeft
+#define CBLAS_RI          CblasRight
+#define CBLAS_NU          CblasNonUnit
+
+double wall_time () {
+   struct timespec ts;
+   clock_gettime(CLOCK_MONOTONIC,&ts);
+   return (double) (ts.tv_sec) + (double) ts.tv_nsec * 1.0e-9;
+}
+
+static type_t pow_di(type_t x, int n)
+{
+   type_t rv = 1.0;
+
+   if (n < 0) {
+      n = -n;
+      x = 1.0 / x;
+   }
+
+   for (; n; n >>= 1, x *= x) {
+      if (n & 1) rv *= x;
+   }
+
+   return rv;
+}
+
+static void gather_block(const int N, type_t *Alin, type_t *A)
+{
+   for (int i = 0; i < ts; i++) {
+      for (int j = 0; j < ts; j++) {
+         A[i*ts + j] = Alin[i*N + j];
+      }
+   }
+}
+
+static void scatter_block(const int N, type_t *A, type_t *Alin)
+{
+   for (int i = 0; i < ts; i++) {
+      for (int j = 0; j < ts; j++) {
+         Alin[i*N + j] = A[i*ts + j];
+      }
+   }
+}
+
+static void convert_to_blocks(const int DIM, const int N, type_t (*Alin)[N], type_t *A[DIM][DIM])
+{
+   for (int i = 0; i < DIM; i++) {
+      for (int j = 0; j < DIM; j++) {
+         gather_block(N, &Alin[i*ts][j*ts], A[i][j]);
+      }
+   }
+}
+
+static void convert_to_linear(const int DIM, const int N, type_t *A[DIM][DIM], type_t (*Alin)[N])
+{
+   for (int i = 0; i < DIM; i++) {
+      for (int j = 0; j < DIM; j++) {
+         scatter_block(N, A[i][j], (type_t *) &Alin[i*ts][j*ts]);
+      }
+   }
+}
